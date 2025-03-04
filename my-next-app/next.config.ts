dbed6f15fad0dcb2ba2b924e7433f5c4a613da88
UdeeshaRukshan <IT21807480@my.sliit.lk>
2025-03-04 16:51:12 +0530

    fix deployment issue

--- my-next-app/next.config.ts
+++ my-next-app/next.config.ts
@@ -1,19 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Enable static export
-  distDir: 'out', // Change output folder (default is 'out')
-  
-  // Set the base path for the GitHub Pages deployment (replace with your repository name)
-  basePath: '/myPersona', // E.g. '/nextjs-persona'
-  assetPrefix: '/myPersona/', // E.g. '/nextjs-persona/'
-  
-  // Disable Next.js Image Optimization for GitHub Pages (as it doesn't support SSR)
-  images: {
-    unoptimized: true, // Disable image optimization
-  },
-
-  // Optional: You can set up custom headers, redirects, etc.
+  /* config options here */
 };
 
 export default nextConfig;
