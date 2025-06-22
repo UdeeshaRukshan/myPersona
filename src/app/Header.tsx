abd5821fc1478b0c9fbae55abd7090f1cf3b1dac
UdeeshaRukshan <IT21807480@my.sliit.lk>
2025-06-22 13:45:32 +0530

    comment added to the Header component to check the deployment issue

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Menu } from "lucide-react"; // Import the menu icon
-
+//Header
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
